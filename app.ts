2758ac9826abd6b497a4da866d078c8be74ec0b4
indrani321 <131009169+indrani321@users.noreply.github.com>
2023-07-10 14:46:11 +0530

    tsc second

--- app.ts
+++ app.ts
@@ -1,45 +1,12 @@
-const num1Element = document.getElementById('num1') as HTMLInputElement
-const num2Element = document.getElementById('num2') as HTMLInputElement
-const buttonElement = document.querySelector('button')!;
+import express from 'express'
+import bodyParser from 'body-parser';
 
-const numResult: Array<number>=[];
-const textResult: string[] =[];
+const app= express();
 
-type numOrString = number|string;
-type Result = {val:number;timestamp:Date}
+app.use(bodyParser.json())
 
-function add(num1:numOrString,num2:numOrString){
-    if(typeof num1==='number'&& typeof num2==='number')
-    {
-        return num1+num2; 
-    }
-    else if(typeof num1==='string'&& typeof num2==='string'){
-        return num1+' '+num2;
-    }
-    return +num1+ +num2;
-}
+import todosRoute from './routes/todos';
 
-function printResult(resultobj:Result){
-    console.log(resultobj.val)
-}
+app.use(todosRoute);
 
-
-buttonElement.addEventListener('click',()=>{
-    const num1 = num1Element.value;
-    const num2= num2Element.value;
-    const result =add(+num1,+num2);
-    numResult.push(result as number)
-    const stringResult = add(num1,num2)
-    textResult.push(stringResult as string)
-    printResult({val:result as number,timestamp:new Date()})
-    console.log(numResult,textResult);
-})
-
-const myPromise = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        resolve('It worked!');
-    },1000)
-})
-myPromise.then((result)=>{
-    console.log(result);
-})
+app.listen(3000);
